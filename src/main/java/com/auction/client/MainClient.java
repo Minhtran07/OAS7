04873b0e72b718b69844b5c0fb85117a90c0b7cb
Viet Anh <vietanhdao643@gmai.com>
2026-04-09 17:31:20 +0700

    first demo

--- src/main/java/com/auction/client/MainClient.java
+++ src/main/java/com/auction/client/MainClient.java
@@ -1,4 +1,22 @@
 package com.auction.client;
 
-public class MainClient {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class MainClient extends Application {
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        FXMLLoader fxmlLoader = new FXMLLoader(MainClient.class.getResource("/client/fxml/login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setTitle("Auction Client");
+        stage.setScene(scene);
+        stage.show();
+    }
 }

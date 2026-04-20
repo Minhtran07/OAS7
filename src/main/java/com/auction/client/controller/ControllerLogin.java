407d96b267be1aef0da5daff16d8c3eb8ca2256a
Viet Anh <vietanhdao643@gmai.com>
2026-04-20 14:56:33 +0700

    update login and register

--- src/main/java/com/auction/client/controller/ControllerLogin.java
+++ src/main/java/com/auction/client/controller/ControllerLogin.java
@@ -1,5 +1,24 @@
 package com.auction.client.controller;
 
+import com.auction.client.MainClient;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class ControllerLogin {
 
+    public void switchToRegister(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(
+                MainClient.class.getResource("/client/fxml/register.fxml")
+        );
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
--- src/main/java/com/auction/client/controller/ControllerRegister.java
+++ src/main/java/com/auction/client/controller/ControllerRegister.java
@@ -1,4 +1,23 @@
 package com.auction.client.controller;
 
+import com.auction.client.MainClient;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import java.io.IOException;
+
 public class ControllerRegister {
+
+    public void switchToLogin(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(
+                MainClient.class.getResource("/client/fxml/login.fxml")
+        );
+        Stage stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(new Scene(root));
+        stage.show();
+    }
 }
